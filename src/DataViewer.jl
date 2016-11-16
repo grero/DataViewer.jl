d4820a935080bea74e93bf530ca1c38a97208ffc
roger <roger.herikstad@gmail.com>
2016-11-16 09:22:40 +0800

    Removes commented code

--- src/DataViewer.jl
+++ src/DataViewer.jl
@@ -16,12 +16,6 @@ function viewdata(data::Array{Float64,1},t::AbstractArray{Float64,1}=linspace(0,
 	window = glscreen("DataViewer", resolution=(1024,800))
 	println(window)
 	res = widths(window)
-#	window_height = map(window.inputs[:window_area]) do _area
-#		_area.h -40
-#	end 
-#	window_width = map(window.inputs[:window_area]) do _area
-#		_area.w -20
-#	end
 	h = res[2]-40 #ymargins
 	mi,mx = extrema(data)
 	Δx = mx-mi
@@ -29,14 +23,8 @@ function viewdata(data::Array{Float64,1},t::AbstractArray{Float64,1}=linspace(0,
 	for i in 1:length(data)
 		points[i] = Point2f0(10.0 + (i-1)*Δt, h*(data[i]-mi)/Δx + 20)
 	end
-	#_scale = scalematrix(Vec3f0(1.5, 1.0, 1.0))
 	@materialize mouseposition, mouse_buttons_pressed,mouse_button_down, mouse_button_released,scroll = window.inputs
-	#left_pressed  = const_lift(GLAbstraction.pressed, mouse_buttons_pressed, GLFW.MOUSE_BUTTON_LEFT)
-	#left_released = const_lift(x->x!=0, mouse_button_released)
-	#left_down = const_lift(x->x!=0, mouse_button_down)
-	#xytranslate   = GLAbstraction.dragged(mouseposition, left_pressed, true)
-	#start_position = filterwhen(left_down, Vec2f0(0.0), mouseposition)
-	#end_position = filterwhen(left_released, Vec2f0(0.0), mouseposition)
+
 	fscroll = droprepeats(map(Vec2f0, scroll))
 	pan = foldp(+, Vec2f0(0.0),  fscroll)
 
@@ -78,6 +66,5 @@ function viewdata(data::Array{Float64,1},t::AbstractArray{Float64,1}=linspace(0,
 	_view(visualize(value(selection_rectangle), model=end_position), window)
 	renderloop(window)
 end
-
 
 end#moduel
