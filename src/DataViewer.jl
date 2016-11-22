d7c2b1f35e2973ce1e416c3a65cf5c7ba8153982
roger <roger.herikstad@gmail.com>
2016-11-22 17:42:14 +0800

    Loosen the type signature for input data

--- src/DataViewer.jl
+++ src/DataViewer.jl
@@ -7,7 +7,7 @@ Plot `data`. Zoom the horizontal axis by dragging the mouse, pan horizontal by s
 
 	function viewdata(data::Array{Float64,1},t::AbstractArray{Float64,1}=linspace(0,1,length(data)))
 """
-function viewdata(data::Array{Float64,1},t::AbstractArray{Float64,1}=linspace(0,1,length(data)))
+function viewdata{T<:Real}(data::Array{T,1},t::AbstractArray{Float64,1}=linspace(0,1,length(data)))
 	window = glscreen("DataViewer", resolution=(1024,800))
 	res = widths(window)
 	h = res[2]-40 #ymargins
